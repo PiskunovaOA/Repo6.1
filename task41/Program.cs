48b5ec70d490811f1959b45bbb2a92a95f9b4efa
Olga <supernatural64@mail.ru>
2022-08-30 13:43:58 +0400

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,36 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+int[] numbers = new int[10];
+FillArray(numbers);
+PrintNumber(numbers);
+PrintArray(numbers);
+
+
+void PrintNumber (int[] number)
+
+{
+    int count = 0;
+    for (int i = 0; i < number.Length; i++)
+{
+    if (number[i] > 0) count ++;
+}
+    Console.WriteLine($" Колличество чисел больше 0: {count}");
+}
+
+void FillArray(int[] number)
+{
+for(int i = 0; i < number.Length; i++)
+{
+number[i] = new Random().Next(0,10);
+}
+}
+
+void PrintArray(int[] number)
+{
+for(int i = 0; i < number.Length; i++)
+{
+Console.Write(number[i] + " ");
+}
+}
